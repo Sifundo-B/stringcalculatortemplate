42e1d5601d65fd7bf37e79910f4808765d166462
Sifundo-B <sifundo.shenge12@gmail.com>
2024-04-16 09:46:22 +0200

    third attempt

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,19 @@
 class Calculator{
     add(numbers){
-
+        
+        if(!numbers){
+            return 0;
+        }
+        
+            let myarr = numbers.split(/[\n,]/ );
+            //'=', ',', ':'
+            let sum =0;
+            for (let index = 0; index < myarr.length; index++) {
+               sum += parseInt(myarr[index]);
+                
+            }
+            return sum;
+        
     }
 }
 
